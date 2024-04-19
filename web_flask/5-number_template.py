edbd25149176a8dfd0ccd25f0c5f10f75d90037c
reela <sudanrela123@gmail.com>
2024-04-19 09:52:00 +0000

    tst

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,45 +1,43 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+""" starts a Flask web application"""
 
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def helo():
+    """return hello HBNB"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """return HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+def c_isfun(text):
+    """display c followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+def python_iscool(text='is cool'):
+    """display -pyhon, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def imanumber(n):
-    """display “n is a number” only if n is an integer"""
+def is_numb(n):
+    """display n is a number in case only if n is an integer"""
     return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbersandtemplates(n):
+def numb_templates(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+"""starts a Flask web application"""
 
 from flask import Flask, render_template
 from models import *
@@ -11,14 +9,14 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """display the states and cities listed in alphabetical order"""
+    """display states and cities in order"""
     states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """closes the storage on teardown"""
+    """close storage on teardown"""
     storage.close()
 
 if __name__ == '__main__':
